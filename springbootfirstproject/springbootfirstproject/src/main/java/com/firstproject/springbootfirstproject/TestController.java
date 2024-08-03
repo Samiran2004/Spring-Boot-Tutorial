0c1120bfb90941d5f462537b8707654c785d5dcb
Samiran Samanta <130337647+Samiran2004@users.noreply.github.com>
2024-08-03 09:38:50 +0530

    first commit

---
+++ springbootfirstproject/springbootfirstproject/src/main/java/com/firstproject/springbootfirstproject/TestController.java
@@ -0,0 +1,21 @@
+package com.firstproject.springbootfirstproject;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+public class TestController {
+
+    @RequestMapping("/test")
+    public String firstHandler(){
+        return "Hello World, This is just for testing";
+    }
+
+    @RequestMapping("/hello")
+    public String secondHandler(){
+        return "Hello User!!";
+    }
+}
